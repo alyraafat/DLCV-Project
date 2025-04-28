981285611e8f39fb2e5faf267ee579749b977a59
alyraafat <alewaraafat@gmail.com>
2025-04-28 03:05:44 +0300

    fix : fix imports for resnet18

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,4 @@
 from .data_prep import *
 from .eval_metrics import *
 from .utils import *
+from .pretrained_models import *
--- second_scripts/__init__.py
+++ second_scripts/__init__.py
@@ -1,5 +1,4 @@
 from .dataset import *
-from .pretrained_models import *
 from .SecondModel import *
 from .train import *
 from .SecondModelNew import *
